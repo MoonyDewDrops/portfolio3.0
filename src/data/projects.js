62b5aabcf002b3ad69481930af6ba3608ba61caa
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2026-06-28 23:00:35 +0200

    Intergration1
    Set over all the translations, plus the files, project files + descriptions and got a start on making it viewable, though im having trouble with actually letting it be viewed as im getting errors to do with about not having a default. Will have to look into this more

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+import annex from "../assets/images/annexbios.webp";
+import focus from "../assets/images/focus6.webp";
+import remote from "../assets/images/remotecontrol.webp";
+import happy from "../assets/images/happyherbivore.webp";
+import portfolio from "../assets/images/oldportfolio.webp";
+
+export const projects = [
+  {
+    title: "Remote Control",
+    featured: true,
+    image: remote,
+    tech: ["Python", "Flask", "Socket.IO", "JavaScript"],
+    description: "Een browsergebaseerd spel-lobbiesysteem waar gebruikers multiplayer-sessies kunnen hosten of eraan kunnen deelnemen. Bevat momenteel een reactietijdspel en is gebouwd om meer spellen te ondersteunen die ik in de toekomst zal maken.",
+    github: "https://github.com/MoonyDewDrops/remoteControl2",
+    demo: "https://remotecontrol2.onrender.com/",
+  },
+
+  {
+    title: "Focus 6",
+    image: focus,
+    tech: ["PHP","MySQL","JavaScript"],
+    description: "Bedrijfswebsite met een zelfgebouwd CMS zodat de klant zelf pagina's kon beheren en toevoegen.",
+    github: "https://github.com/MoonyDewDrops/focus6",
+    demo: "https://u230067.gluwebsite.nl/focus6/?view",        
+  },
+
+  {
+    title: "Happy Herbivore",
+    image: happy,
+    tech:["PHP","JS","Figma"],
+    description:"Een volledig werkend kiosksysteem voor een fictief plantaardig foodmerk, met verbeterd design, taalwisseling en bonprinting op het testapparaat van school.",
+    github: "https://github.com/MoonyDewDrops/happyherbivore",
+    demo: "https://u240660.gluwebsite.nl/kiosk/",
+  },
+
+  {
+    title:"Portfolio v2",
+    image:portfolio,
+    tech:["HTML","CSS","JavaScript"],
+    description:"Mijn oude portfolio, gemaakt met HTML, CSS en JavaScript. Het is een statische website die mijn projecten en vaardigheden laat zien.",
+    github: "https://github.com/MoonyDewDrops/portfolio2.0",
+    demo: "https://juliabrouwer.onrender.com/home",
+  }
+];
