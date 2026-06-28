9d4959f6983573012d658cc165c1196ff738b5e3
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2026-06-28 23:38:24 +0200

    More file structure

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,37 +1,36 @@
 export default function ProjectCard({ project, t, reverse }) {
-  return (
-    <div
-      className={`pf-card pf-project-card ${
-        project.featured ? "pf-featured" : ""
-      } ${reverse ? "reverse" : ""}`}
-    >
-      {/* TEXT */}
-      <div className="pf-project-text">
-        <p className="pf-card-tag">
-          {project.tech.join(" · ")}
-        </p>
+    return (
+        <div
+            className={`pf-card pf-project-card ${project.featured ? "pf-featured" : ""
+                } ${reverse ? "reverse" : ""}`}
+        >
+            {/* TEXT */}
+            <div className="pf-project-text">
+                <p className="pf-card-tag">
+                    {project.tech.join(" · ")}
+                </p>
 
-        <h3>{project.title}</h3>
+                <h3>{project.title}</h3>
 
-        <p>{t.projectsData?.[project.key] || project.description}</p>
+                <p>{t.projectsData?.[project.key] || project.description}</p>
 
-        <div className="pf-tech-pills">
-          {project.tech.map((tech) => (
-            <span key={tech} className="pf-pill">
-              {tech}
-            </span>
-          ))}
+                <div className="pf-tech-pills">
+                    {project.tech.map((tech) => (
+                        <span key={tech} className="pf-pill">
+                            {tech}
+                        </span>
+                    ))}
+                </div>
+
+                <a href={project.demo} target="_blank" rel="noreferrer">
+                    {t.projects.view}
+                </a>
+            </div>
+
+            {/* IMAGE */}
+            <div className="pf-featured-img">
+                <img src={project.image} alt={project.title} />
+            </div>
         </div>
-
-        <a href={project.demo} target="_blank" rel="noreferrer">
-          {t.projects.view}
-        </a>
-      </div>
-
-      {/* IMAGE */}
-      <div className="pf-featured-img">
-        <img src={project.image} alt={project.title} />
-      </div>
-    </div>
-  );
+    );
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,44 +4,44 @@ import happy from "../assets/images/happyherbivore.webp";
 import portfolio from "../assets/images/oldportfolio.webp";
 
 export const projects = [
-  {
-    key : "remote",
-    title: "Remote Control",
-    featured: true,
-    image: remote,
-    tech: ["Python", "Flask", "Socket.IO", "JavaScript"],
-    description: "Een browsergebaseerd spel-lobbiesysteem waar gebruikers multiplayer-sessies kunnen hosten of eraan kunnen deelnemen. Bevat momenteel een reactietijdspel en is gebouwd om meer spellen te ondersteunen die ik in de toekomst zal maken.",
-    github: "https://github.com/MoonyDewDrops/remoteControl2",
-    demo: "https://remotecontrol2.onrender.com/",
-  },
+    {
+        key: "remote",
+        title: "Remote Control",
+        featured: true,
+        image: remote,
+        tech: ["Python", "Flask", "Socket.IO", "JavaScript"],
+        description: "Een browsergebaseerd spel-lobbiesysteem waar gebruikers multiplayer-sessies kunnen hosten of eraan kunnen deelnemen. Bevat momenteel een reactietijdspel en is gebouwd om meer spellen te ondersteunen die ik in de toekomst zal maken.",
+        github: "https://github.com/MoonyDewDrops/remoteControl2",
+        demo: "https://remotecontrol2.onrender.com/",
+    },
 
-  {
-    key: "focus6",
-    title: "Focus 6",
-    image: focus,
-    tech: ["PHP","MySQL","JavaScript"],
-    description: "Bedrijfswebsite met een zelfgebouwd CMS zodat de klant zelf pagina's kon beheren en toevoegen.",
-    github: "https://github.com/MoonyDewDrops/focus6",
-    demo: "https://u230067.gluwebsite.nl/focus6/?view",        
-  },
+    {
+        key: "focus6",
+        title: "Focus 6",
+        image: focus,
+        tech: ["PHP", "MySQL", "JavaScript"],
+        description: "Bedrijfswebsite met een zelfgebouwd CMS zodat de klant zelf pagina's kon beheren en toevoegen.",
+        github: "https://github.com/MoonyDewDrops/focus6",
+        demo: "https://u230067.gluwebsite.nl/focus6/?view",
+    },
 
-  {
-    key: "happy",
-    title: "Happy Herbivore",
-    image: happy,
-    tech:["PHP","JS","Figma"],
-    description:"Een volledig werkend kiosksysteem voor een fictief plantaardig foodmerk, met verbeterd design, taalwisseling en bonprinting op het testapparaat van school.",
-    github: "https://github.com/MoonyDewDrops/happyherbivore",
-    demo: "https://u240660.gluwebsite.nl/kiosk/",
-  },
+    {
+        key: "happy",
+        title: "Happy Herbivore",
+        image: happy,
+        tech: ["PHP", "JS", "Figma"],
+        description: "Een volledig werkend kiosksysteem voor een fictief plantaardig foodmerk, met verbeterd design, taalwisseling en bonprinting op het testapparaat van school.",
+        github: "https://github.com/MoonyDewDrops/happyherbivore",
+        demo: "https://u240660.gluwebsite.nl/kiosk/",
+    },
 
-  {
-    key: "portfolio",
-    title:"Portfolio v2",
-    image:portfolio,
-    tech:["HTML","CSS","JavaScript"],
-    description:"Mijn oude portfolio, gemaakt met HTML, CSS en JavaScript. Het is een statische website die mijn projecten en vaardigheden laat zien.",
-    github: "https://github.com/MoonyDewDrops/portfolio2.0",
-    demo: "https://juliabrouwer.onrender.com/home",
-  }
+    {
+        key: "portfolio",
+        title: "Portfolio v2",
+        image: portfolio,
+        tech: ["HTML", "CSS", "JavaScript"],
+        description: "Mijn oude portfolio, gemaakt met HTML, CSS en JavaScript. Het is een statische website die mijn projecten en vaardigheden laat zien.",
+        github: "https://github.com/MoonyDewDrops/portfolio2.0",
+        demo: "https://juliabrouwer.onrender.com/home",
+    }
 ];
